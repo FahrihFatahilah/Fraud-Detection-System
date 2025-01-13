22e773114ee3180bf2f1308c8e7ec7431a44bf8a
Fahrih Fatahilah <fahrihfatahilah@gmail.com>
2025-01-13 14:36:39 +0700

    add equals and hashcode

--- src/main/java/com/rest/fds/entity/TransactionRequest.java
+++ src/main/java/com/rest/fds/entity/TransactionRequest.java
@@ -2,9 +2,11 @@ package com.rest.fds.entity;
 
 import com.rest.fds.entity.Model.BaseResponseModel;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
 public class TransactionRequest<T> extends BaseResponseModel<T> {
 
