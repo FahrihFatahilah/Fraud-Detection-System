6864f2804fdc795c3b43a34ae683c2c2981f6c11
Fahrih Fatahilah <fahrihfatahilah@gmail.com>
2025-01-13 14:32:00 +0700

    fixing raw type class

--- src/main/java/com/rest/fds/util/BaseError.java
+++ src/main/java/com/rest/fds/util/BaseError.java
@@ -6,20 +6,20 @@ import org.springframework.http.ResponseEntity;
 
 public class BaseError<T> {
 
-    public ResponseEntity<BaseResponseModel> createErrorResponse(String errorCode, String errorDescription) {
-        BaseResponseModel baseResponseModel = new BaseResponseModel();
+    public ResponseEntity<BaseResponseModel<T>> createErrorResponse(String errorCode, String errorDescription) {
+        BaseResponseModel<T> baseResponseModel = new BaseResponseModel<>();
         baseResponseModel.setSuccessful(false);
         baseResponseModel.setErrorCode(errorCode);
         baseResponseModel.setErrorDescription(errorDescription);
-        return new ResponseEntity<BaseResponseModel>(baseResponseModel,HttpStatusCode.valueOf(Integer.parseInt(errorCode)));
+        return new ResponseEntity<BaseResponseModel<T>>(baseResponseModel,HttpStatusCode.valueOf(Integer.parseInt(errorCode)));
     }
-    public ResponseEntity<BaseResponseModel> createSuccessResponseResponse(String refNo, String msg, T response) {
+    public ResponseEntity<BaseResponseModel<T>> createSuccessResponseResponse(String refNo, String msg, T response) {
         BaseResponseModel<T> baseResponseModel = new BaseResponseModel<>();
         baseResponseModel.setRefNo(refNo);
         baseResponseModel.setSuccessful(true);
         baseResponseModel.setMessage(msg);
         baseResponseModel.setData(response);
-        return new ResponseEntity<BaseResponseModel>(baseResponseModel,HttpStatusCode.valueOf(200));
+        return new ResponseEntity<BaseResponseModel<T>>(baseResponseModel,HttpStatusCode.valueOf(200));
 
     }
 }
